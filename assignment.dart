ced3321caa4ae9771dc76f3ae1c74834d0130ed4
aqibrajput07 <98934039+aqibrajput07@users.noreply.github.com>
2022-02-09 21:52:53 -0800

    Add files via upload

---
+++ assignment.dart
@@ -0,0 +1,31 @@
+void main(){
+print("Twinkle Twinkle Little Star \n How a Wonder What you are \t Humty Dumty");
+  var firstname="Sajjad"; var lastname="Ahmed";
+  var age=20; var salary=15000;
+  print("First Name : "+firstname);
+  print("Last Name : "+lastname);
+  print("Age : ${age}\nSalary : ${salary}");
+  print("third task");
+  print(firstname.runtimeType);
+  print(age.runtimeType);
+  print("Task 4");
+  int a=16;
+  int b=32;
+  var sum=a+b;
+  print("Sum ${sum}");
+  int c=72;
+  int d=24;
+  var sub=c-d;
+  print("Sub ${sub}");
+  int e=2;
+  int f=24;
+  var mult=e*f;
+  print("Multiplication ${mult}");
+  int g=48;
+  int h=2;
+  var div=g/h;
+  print("Division ${div}");
+
+ 
+}
+
